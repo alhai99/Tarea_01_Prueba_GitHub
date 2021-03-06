f1330a6f73ccb704f3390ac007fcf3e716845928
alhai99 <79435649+alhai99@users.noreply.github.com>
2021-03-05 20:58:15 -0600

    Add files via upload

---
+++ HolaMundo_Ramirez_Reyes_Alhai.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+/* 
+viernes 26 de febrero
+primera tarea POO 
+ramirez reyes alhai 
+*/
+using namespace std;
+int main()
+{
+    cout<<"Hola Alhai Ramirez Reyes"<< endl;
+    getch();
+    return 0;
+}
